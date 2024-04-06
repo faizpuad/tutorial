636439e20fea38d13f225a79a1e2fdc7792b6fc7
MUHAMMAD FAIZ BIN MOHD PUAD <faizpuad@MUHAMMADs-MacBook-Air.local>
2024-04-06 11:36:39 +0800

    The first 3 project files added.

---
+++ project2_fastapi/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# # main.py
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
+
+# main.py
+
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+app = FastAPI()
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
